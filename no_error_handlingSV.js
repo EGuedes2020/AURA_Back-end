8f2419d413526c340831f889182a686781110033
lffsantos24 <lffsantos@ua.pt>
2023-04-26 14:50:42 +0100

    Primeiro middleware para error-handling

--- no_error_handlingSV.js
+++ no_error_handlingSV.js
@@ -13,66 +13,53 @@ const sequelize = new Sequelize('AURA_db', 'postgres', 'postgres', {
 });
 
 //----------------------------------------IMPORT DOS MODELOS
-const Institution = require('./models/institutions')
-const Suggestion  = require('./models/suggestions');
-const Worker = require('./models/workers')
-const Badge = require('./models/badges')
-const Energy = require('./models/energy_consumption')
-const InstitutionBadge = require('./models/institution_badges');
+const Institution = require('./server/models/institutions')
+const Suggestion  = require('./server/models/suggestions');
+const Worker = require('./server/models/workers')
+const Badge = require('./server/models/badges')
+const Energy = require('./server/models/energy_consumption')
+const InstitutionBadge = require('./server/models/institution_badges');
 
 //--------------------------------EXPRESS VALIDATOR
 const { check,query, validationResult } = require('express-validator');
 
-//--------------------------------ERROR-HANDLING MIDDLEWARE
+//1-Listagem de todas as instituições
+app.get('/api/institutions',
+  [
+    query('sortBy').optional().isIn(['name', 'createdAt']),
+    query('sortDirection').optional().isIn(['asc', 'desc']),
+    query('limit').optional().isInt({ min: 1, max: 100 }),
+    query('offset').optional().isInt({ min: 0 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { sortBy = 'name', sortDirection = 'asc', limit = 20, offset = 0 } = req.query;
+
+    try {
+      const institutions = await Institution.findAll({
+        order: [[sortBy, sortDirection.toUpperCase()]],
+        limit,
+        offset,
+      });
+      res.json(institutions);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+    }
+  });
 
 
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack); 
-  let status;
-  let message;
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-    status = 400;
-    message = 'Invalid JSON payload';
-    console.error(err)
-  } else if (err instanceof MyCustomError) {
-    status = 500;
-    message = 'Something went wrong with our servers. We are already working to fix it.';
-    console.error(err)
-  } else if (err.status === 404) {
-    status = 404;
-    message = 'Not foundddd';
-    console.error(err)
-  } else {
-    status = 500;
-    message = 'Internal server error';
-    console.error(err)
-  }
-
-  res.status(status).json({ message });
-  console.log(err)
-};
-
-app.use(errorHandler)
-
-
-//------------------------------ROTAS
-
-// (1) Todas as instituições
-app.get('/api/institutions', async (req, res, next) => {
-  try {
-    const institutions = await Institution.findAll();
-    res.status(200).json(institutions);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// (2) Todas as sugestões
+//2-Listagem de todas as sugestões
 app.get('/api/suggestions', [
   check('institution_id').optional().isInt(),
   check('author_id').optional().isInt(),
-  check('description').optional().isString(),
-], async (req, res, next) => { // add "next" parameter
+  check('description').optional().isString(), //não faz muito sentido porque a descrição é obrigatória, se fizer isso dá erro
+  /*check('description').notEmpty().withMessage('Description is required').isString(),*/ 
+], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
@@ -83,17 +70,18 @@ app.get('/api/suggestions', [
     res.json(suggestions);
   } catch (err) {
     console.error(err);
-    next(err); 
+    res.status(500).send(err.message);
   }
 });
 
-// (3) Todos os trabalhadores
+
+//3-Listagem de todos os trabalhadores
 app.get('/api/workers', [
-  check('name').optional().isLength({ min: 1 }).withMessage('Name is required and must be at least 1 character'),
+check('name').optional().isLength({ min: 1 }).withMessage('Name is required and must be at least 1 character'),
   check('email').isEmail().optional({ nullable: true }),
   check('phone_number').isMobilePhone().optional({ nullable: false }),
   check('role').isLength({ max: 50 }).optional({ nullable: false }),
-], async (req, res, next) => {
+], async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -104,17 +92,17 @@ app.get('/api/workers', [
     res.json(workers);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
 
-// (4) Todos os badges
+//4-Listagem de todos os Badges
 app.get('/api/badges', [
-  check('name').optional().notEmpty(),
-  check('season').optional(),
-  check('year').optional().isInt(),
-  check('description').optional(),
-], async (req, res, next) => {
+ check('name').optional().notEmpty(),
+ check('season').optional(),
+ check('year').optional().isInt(),
+ check('description').optional(),
+], async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -125,18 +113,15 @@ app.get('/api/badges', [
     res.json(badges);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
-}, function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal server error' });
 });
 
-// (5) Todos os consumos de energia
+//5-Listagem de todos os Consumos de Energia
 app.get('/api/energy', [
-  check('year').optional().isInt({ min: 1900, max: 2100 }),
-  check('month').optional().isInt({ min: 1, max: 12 }),
-], async (req, res, next) => {
+ check('year').optional().isInt({ min: 1900, max: 2100 }),
+ check('month').optional().isInt({ min: 1, max: 12 }),
+], async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -145,18 +130,19 @@ app.get('/api/energy', [
     
     const energy = await Energy.findAll();
     res.json(energy);
-    console.log(energy)
+    console.log(ener)
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
 
-// (6) Histórico de Badges atribuídos a instituições
+
+//6-Listagem de badges das instituições
 app.get('/api/institutions-badges', [
   check('institution_id').optional().isInt(),
   check('badge_id').optional().isInt(),
-], async (req, res, next) => {
+], async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -167,21 +153,22 @@ app.get('/api/institutions-badges', [
     res.json(institutionBadges);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
+;
 InstitutionBadge.removeAttribute('id')
 
-// (7) Trabalhadores de uma instituição
+//7- Listagem de trabalhadores de uma instituição
 app.get('/api/institutions/:id/workers', [
   check('id').isInt().toInt(),
 ], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const institutionId = req.params.id;
     const institution = await Institution.findOne({ where: { id: institutionId } });
     if (!institution) {
@@ -202,11 +189,12 @@ app.get('/api/institutions/:id/workers', [
     res.json(formattedWorkers);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
 
-// (8) Sugestões de uma instituição
+
+//8-Listagem de Sugestões de uma instituição
 app.get('/api/institutions/:id/suggestions', [
   check('id').isInt().withMessage('Invalid institution ID'),
 ], async (req, res) => {
@@ -223,11 +211,12 @@ app.get('/api/institutions/:id/suggestions', [
     res.json(suggestions);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
 
-// (9) Consumos de energia de uma instituição
+
+//9-Consumos de energia de uma instituição
 app.get('/api/institutions/:id/energy', [
   check('id').isInt(),
 ], async (req, res) => {
@@ -244,11 +233,11 @@ app.get('/api/institutions/:id/energy', [
     res.json(result);
   } catch (err) {
     console.error(err);
-    next(err);
+    res.status(500).send(err.message);
   }
 });
 
-// (10) Badges de uma instituição
+//10-Badges de uma instituição
 app.get('/api/institutions/:id/badges', [
   check('id').isInt(),
 ], async (req, res) => {
@@ -268,8 +257,6 @@ app.get('/api/institutions/:id/badges', [
     res.status(500).send(err.message);
   }
 });
-
-
 
 
 //-----------------------------TESTE DE LIGAÇÃO--------------------------------

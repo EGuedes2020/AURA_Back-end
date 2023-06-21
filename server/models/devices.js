3075bb20f0c3877aee4a11edeb3c2716b156c957
lffsantoss <96009520+lffsantoss@users.noreply.github.com>
2023-06-21 12:14:04 +0100

    introdução dos dispositivos

---
+++ server/models/devices.js
@@ -0,0 +1,43 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('AURA_db', 'postgres', 'postgres', {
+  host: 'aura-database-instance.cts91ecvtypq.eu-north-1.rds.amazonaws.com',
+  dialect: 'postgres'
+});
+
+const Device = sequelize.define('Device', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    institution_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    room: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    name_of_device: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    assigned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    institution_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    room_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  }, {
+    tableName: 'devices',
+    timestamps: false,
+  });
+  
+  module.exports = Device;
